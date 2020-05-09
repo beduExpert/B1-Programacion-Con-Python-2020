1b9f910ad769380b88a53842a241679eb0f408b3
Luis Menendez Salazar <menendez.sla@gmail.com>
2020-05-09 18:16:44 -0500

    se agregan primeros retos

---
+++ Sesion-01/Ejemplo-04/cadenas.py
@@ -0,0 +1,21 @@
+
+# Esto es una cadena                                                                                                                                                                 
+d = "Hola mundo"                                                                                                                                                                     
+
+# Podemos comprobar el dato con type(), en inglés se les conoce como string                                                                                                                                              
+print(type(d))
+
+# Podemos utilizar comillas simples, o tres comillas dobles                                                                                                                         
+e = 'Saludos'
+f = """What's your name?"""
+
+#Las cadenas pueden tener unicamente 1 caracter, o incluso 0
+un_caracter = 'a'
+print(type(un_caracter))
+
+cadena_vacia = ''
+print(type(cadena_vacia))
+
+# Operaciones con cadenas
+print("Hola" * 5) #'* Repite la misma cadena n-veces'
+print("Hola" + " Mundo") #'+ concatena cadenas'
