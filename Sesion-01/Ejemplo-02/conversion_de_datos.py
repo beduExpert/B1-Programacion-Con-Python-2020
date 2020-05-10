ba9dbce6d0202f5a9b75fdecd9667074ab25370e
Luis Menendez Salazar <menendez.sla@gmail.com>
2020-05-09 19:20:22 -0500

    add ejemplo convertir tipos

---
+++ Sesion-01/Ejemplo-02/conversion_de_datos.py
@@ -0,0 +1,17 @@
+#Se puede definir números como cadenas si se encierran en comillas
+numero1 = "100"
+numero2 = "3.14159"
+print(type(numero1))
+
+#Para convertir a entero 
+entero = int(numero1)
+print(type(entero))
+
+#Para convertir a flotante
+flotante = float(numero2)
+print(type(flotante))
+
+#También se puede convertir un número a cadena de texto
+num = 300
+cadena = str(num)
+print(type(cadena))
