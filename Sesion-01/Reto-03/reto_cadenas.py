731df81650df42c02fc52adec73e699849d559c0
Luis Menendez Salazar <menendez.sla@gmail.com>
2020-05-09 20:08:57 -0500

    Edicion a reto.py

--- Sesion-01/Reto-03/reto_cadenas.py
+++ Sesion-01/Reto-03/reto_cadenas.py
@@ -1,2 +1,23 @@
-cadena1 = input()
-cadena2 = input()
+print("¿Qué tabla quieres calcular?")
+numero = int(input())
+numero2 = 1
+
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
+numero2 += 1
+print("{} * {} = {}".format(numero, numero2, numero*numero2) )
